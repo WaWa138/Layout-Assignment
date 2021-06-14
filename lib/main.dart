8e55dd6b3cdbdab5c170ae36dafab9e8c180deb4
WaWa138 <49987003+WaWa138@users.noreply.github.com>
2021-06-14 11:02:07 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,127 @@
+import 'package:flutter_switch/flutter_switch.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main(List<String> args) {
+  runApp(new MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+
+
 
 class MyApp extends StatefulWidget {
+
   @override
   _MyAppState createState() => _MyAppState();
+  bool status = false;
 }
 
 class _MyAppState extends State<MyApp> {
-  bool _isBluetoothOn = false;
+  bool _value = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text("MENU UTAMA"),
+        backgroundColor: Colors.green[700],
+      ),
+      backgroundColor: Colors.green[100],
+      body: Container(
+        padding: EdgeInsets.all(30.0),
+        child: GridView.count(crossAxisCount: 2, children: <Widget>[
+          MyMenu(
+              title: "LOCATION",
+              icon: Icons.add_location_outlined,
+              warna: Colors.black),
+          MyMenu(
+              title: "SNOW",
+              icon: Icons.ac_unit_rounded,
+              warna: Colors.blue),
+          MyMenu(
+              title: "DARK",
+              icon: Icons.bedtime,
+              warna: Colors.black),
+          MyMenu(
+              title: "SUNNY",
+              icon: Icons.brightness_5_outlined,
+              warna: Colors.deepOrange),
+          MyMenu(
+              title: "CLOUDY",
+              icon: Icons.cloud_circle_outlined,
+              warna: Colors.lightBlue),
+          MyMenu(
+              title: "RAINY",
+              icon: Icons.beach_access,
+              warna: Colors.blueGrey,
+    ),
+
+
+              //_value: true,
+
+
+        ]
+      ),
+      ),
+    );
+  }
+}
+
+
+class MyMenu extends StatelessWidget {
+  MyMenu({this.title, this.icon, this.warna});
+
+  final String title;
+  final IconData icon;
+  final MaterialColor warna;
+
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter IconButon Tutorial by Woolha.com'),
-        ),
-        body: Center(
-          child: IconButton(
-            icon: Icon(Icons.bluetooth),
-            onPressed: () {
-              setState(() {
-                _isBluetoothOn = !_isBluetoothOn;
-              });
-            },
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      child: InkWell(
+        onTap: () {},
+        splashColor: Colors.green,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Icon(
+                icon,
+                size: 70.0,
+                color: warna,
+              ),
+              Text(title, style: new TextStyle(fontSize: 17.0)),
+             /*Center(
+               child: Container(
+                 child: FlutterSwitch(
+                   width: 125.0,
+                   height: 55.0,
+                   valueFontSize: 25.0,
+                   toggleSize: 45.0,
+                   value: status,
+                   borderRadius: 30.0,
+                   padding: 8.0,
+                   showOnOff: true,
+                   onToggle: (val) {},
+                 ),
+               ),
+             )*/
+              Center(
+                child: Switch(
+                  value: false,
+                  onChanged: (bool value) {
+                    //setState(() {
+                     // _value = value;
+                    //});
+                  },
+                  activeTrackColor: Colors.lightGreenAccent,
+                  activeColor: Colors.green,
+                ),
+              )
+            ], // activeTrackColor: Colors.lightGreenAccent,
+            //  activeColor: Colors.green,
           ),
         ),
       ),
